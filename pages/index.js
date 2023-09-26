a2a40ab1353da1dd53f51b265b753ee4fb2cea13
Vitalii Berko <vitalii.v.berko@gmail.com>
2023-09-26 21:09:16 +0300

    Updated main page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
 import Link from "next/link";
+import Head from "next/head";
 
 export default function Index() {
   return (
     <>
+    <Head>
+    <link rel="icon" type="image/svg+xml" href="/logo.svg" />
+      <title>Next.js-course</title>
+      <meta name="keywords" content="next, js, javascript, next.js, nextjs"></meta>
+      <meta name="description" content="next, js, javascript, next.js, nextjs"></meta>
+    </Head>
       <h1>Hello Next.js</h1>
       <p>
         <Link href='/about'>About</Link>
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,8 +1,12 @@
+import Head from "next/head";
 import  Router from "next/router";
 
 export default function Posts() {
   return (
     <>
+    <Head>
+      <title> Posts page</title>
+    </Head>
       <h1>Posts Page</h1>
       <button onClick={() => Router.back()}>Go back to home</button>
     </>
