b51f1ba117c459622fd5af7c6e91784216f2faaf
Vitalii Berko <vitalii.v.berko@gmail.com>
2023-10-04 17:06:11 +0300

    Updated

--- pages/404.js
+++ pages/404.js
@@ -1,9 +1,10 @@
 import Link from "next/link";
+import css from '../styles/error.module.css'
 
 export default function ErrorPage() {
     return(
         <>
-        <h1>Erro page</h1>
+        <h1 className={css.error}>Erro page</h1>
         <p>Please <Link href={'/'}> go back</Link> to safety page</p>
         </>
     )
